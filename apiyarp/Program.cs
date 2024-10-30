b85cf1799f5f92758638b3a3102a523fb39248e1
PierreGode <pierre@gode.one>
2024-10-30 15:01:25 +0100

    added /instance

--- apiyarp/Program.cs
+++ apiyarp/Program.cs
@@ -4,10 +4,19 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add YARP to the services without calling AddActiveHealthChecks()
+// Add YARP to the services
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
+
+// Create an endpoint to return the application name and instance ID
+app.MapGet("/instance", () =>
+{
+    string instanceId = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") ?? "Instance ID not found";
+    string appName = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "App Name not found";
+    return Results.Ok(new { source = appName, InstanceId = instanceId });
+});
 
 // Enable YARP middleware
 app.MapReverseProxy();
