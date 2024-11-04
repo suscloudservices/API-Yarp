d045a0e3b5493aa6bd34fb24ccd9abcdd0adec83
PierreGode <pierre@gode.one>
2024-11-04 15:36:26 +0100

    Update Program.cs

--- apiyarp/Program.cs
+++ apiyarp/Program.cs
@@ -10,11 +10,23 @@ builder.Services.AddReverseProxy()
 
 var app = builder.Build();
 
+app.UseMiddleware<CountryRestrictionMiddleware>();
+
 app.MapGet("/instance", () =>
 {
     string instanceId = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") ?? "Instance ID not found";
     string appName = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "App Name not found";
     return Results.Ok(new { source = appName, InstanceId = instanceId });
+});
+
+app.MapGet("/show-headers", async (HttpContext context) =>
+{
+    var headers = context.Request.Headers;
+    foreach (var header in headers)
+    {
+        Console.WriteLine($"{header.Key}: {header.Value}");
+    }
+    return Results.Json(headers);
 });
 
 app.MapReverseProxy();
